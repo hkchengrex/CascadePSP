83cc3b8783b595b2e47c75016f93654eaddb7412
Rex <hkchengrex@gmail.com>
2022-08-19 11:37:53 -0500

    remove hashlib

--- segmentation-refinement/setup.py
+++ segmentation-refinement/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="segmentation-refinement",
-    version="0.5",
+    version="0.6",
     author="Ho Kei Cheng, Jihoon Chung",
     author_email="hkchengrex@gmail.com",
     description="Deep learning based segmentation refinement system.",
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['torch', 'torchvision', 'requests', 'hashlib'],
+    install_requires=['torch', 'torchvision', 'requests'],
 )
